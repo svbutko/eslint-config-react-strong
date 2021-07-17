1a8d1a1ae7386c7bd20f5f9bb4f8a2acb19c796b
svbutko <svbutko@hotmail.com>
2021-07-17 17:53:50 +0300

    Modify existing rules based on feedback

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = {
     rules: {
         //react-native
         "react-native/no-unused-styles": "error",
-        "react-native/split-platform-components": "error",
+        "react-native/split-platform-components": "off",
         "react-native/no-inline-styles": "error",
         "react-native/no-color-literals": "error",
         "react-native/no-raw-text": "error",
@@ -33,21 +33,23 @@ module.exports = {
         ],
 
         //typescript-eslint
+        "@typescript-eslint/strict-boolean-expressions": "off",
+        "max-classes-per-file": "off",
+        "@typescript-eslint/unbound-method": "off",
+        "@typescript-eslint/no-shadow": ["error"],
+
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/array-type": "error",
         "@typescript-eslint/await-thenable": "error",
-        "@typescript-eslint/class-name-casing": "error",
         "@typescript-eslint/consistent-type-assertions": "error",
         "@typescript-eslint/consistent-type-definitions": "error",
-        "@typescript-eslint/indent": "error",
-        "@typescript-eslint/interface-name-prefix": "error",
+        "@typescript-eslint/indent": "off",
         "@typescript-eslint/member-delimiter-style": "error",
         "@typescript-eslint/no-empty-function": "error",
         "@typescript-eslint/no-for-in-array": "error",
         "@typescript-eslint/no-misused-new": "error",
         "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/no-unnecessary-qualifier": "error",
-        "@typescript-eslint/no-use-before-declare": "error",
         "@typescript-eslint/no-var-requires": "error",
         "@typescript-eslint/prefer-for-of": "error",
         "@typescript-eslint/prefer-namespace-keyword": "error",
@@ -57,10 +59,8 @@ module.exports = {
             "error",
             "always"
         ],
-        "@typescript-eslint/strict-boolean-expressions": "error",
         "@typescript-eslint/triple-slash-reference": "error",
         "@typescript-eslint/type-annotation-spacing": "error",
-        "@typescript-eslint/unbound-method": "error",
         "@typescript-eslint/unified-signatures": "error",
         "camelcase": "error",
         "complexity": "error",
@@ -80,7 +80,6 @@ module.exports = {
             "Undefined",
         ],
         "id-match": "error",
-        "max-classes-per-file": "error",
         "max-len": [
             "error",
             {
@@ -94,19 +93,14 @@ module.exports = {
         "no-empty": "error",
         "no-eval": "error",
         "no-fallthrough": "error",
-        "no-invalid-this": "error",
+        "no-invalid-this": "off",
         "no-multiple-empty-lines": "error",
         "no-new-wrappers": "error",
         "no-redeclare": "error",
-        "no-shadow": [
-            "error",
-            {
-                "hoist": "all"
-            }
-        ],
+        "no-shadow": "off",
         "no-trailing-spaces": "error",
         "no-undef-init": "error",
-        "no-underscore-dangle": "error",
+        "no-underscore-dangle": "off",
         "no-unsafe-finally": "error",
         "no-unused-expressions": [
             "error",
